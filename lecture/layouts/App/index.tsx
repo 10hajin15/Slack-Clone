7ad38668aa1ea6c3c00294695f7d85bd870f185a
Jinny <10haijn15@gmail.com>
2023-05-08 15:04:17 +0900

    ⚡️ 코드 스플리팅

--- lecture/layouts/App/index.tsx
+++ lecture/layouts/App/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import loadable from '@loadable/component'
 import { Redirect, Route, Switch } from 'react-router-dom';
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
 
-// const LogIn = loadable(() => import('@pages/LogIn'));
-// const SignUp = loadable(() => import('@pages/SignUp'));
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
+const Channel = loadable(() => import('@pages/Channel'));
 
 const App = () => {
   return (
@@ -13,6 +12,7 @@ const App = () => {
       <Redirect exact path="/" to="/login" />
       <Route path='/login' component={LogIn} />
       <Route path='/signup' component={SignUp} />
+      <Route path='/workspace' component={Channel} />
     </Switch>
   );
 };
---
+++ lecture/pages/Channel/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Channel = () => {
+  return (
+    <div>로그인 축하축하</div>
+  );
+}
+
+export default Channel;
